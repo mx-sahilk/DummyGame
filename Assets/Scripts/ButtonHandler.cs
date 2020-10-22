3ec08d784cf69fa5bfe5fa0ac65cd53bd78f715d
Sahil Kansal <sahil.kansal@mxplayer.in>
2020-10-22 16:17:15 +0530

    changes

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ButtonHandler : MonoBehaviour
 {
     public void onFinish(){
+       Debug.Log(NativeInterfaceImpl.userId);
        NativeInterfaceImpl scene1 = GameObject.Find("GameManager").GetComponent<NativeInterfaceImpl>();
          scene1.onGameEnd("55");
     }
--- Assets/Scripts/NativeInterfaceImpl.cs
+++ Assets/Scripts/NativeInterfaceImpl.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Dummy scene component
 public class NativeInterfaceImpl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     public static string userId;
     
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
-        //onGameStart("123");   
+        onGameStart("123");   
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
+    //For starting the game with all required data to the game
+    // param id is player id
     public void onGameStart(string id){
          userId = id;
          launchGame();
@@ -29,6 +27,7 @@ public class NativeInterfaceImpl : MonoBehaviour
         SceneManager.LoadScene("MXGame", LoadSceneMode.Additive);
     }
 
+    //this method is called to post score to native when game finished
     public void onGameEnd(string score){
         Debug.Log(score);
         AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
